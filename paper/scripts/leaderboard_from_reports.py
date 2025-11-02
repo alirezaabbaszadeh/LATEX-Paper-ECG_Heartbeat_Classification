128d88b125b98967cdabea7f6dab0034d31d5dab
ALIREZA ABBASZADEH <abbaszadeh79@gmail.com>
2025-11-02 18:08:22 +0330

    Add paper scripts: figure sync + stubs for leaderboard, calibration, appendix

---
+++ paper/scripts/leaderboard_from_reports.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# TODO: implement leaderboard extraction to CSV/TeX.
+# placeholder to keep path stable in this PR.
+print('leaderboard_from_reports.py placeholder')
---
+++ paper/scripts/make_appendix_figs.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# TODO: implement appendix packer.
+print('make_appendix_figs.py placeholder')
